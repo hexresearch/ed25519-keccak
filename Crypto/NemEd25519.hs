8d5c6bca336739031a61973facf5a38b83c570f0
Sergey Ivanov <ivanovs-4@yandex.ru>
2018-04-18 00:59:41 +0400

    Hide modules, reexport in Crypto.NemEd25519

---
+++ Crypto/NemEd25519.hs
@@ -0,0 +1,12 @@
+-- |
+-- Public Reimports
+-- Everything else is hidden
+--
+module Crypto.NemEd25519 (
+      module X
+    ) where
+
+import Crypto.Error                       as X
+import Crypto.Hash                        as X
+import Crypto.Hash.Algorithms             as X
+import Crypto.PubKey.Ed25519              as X
