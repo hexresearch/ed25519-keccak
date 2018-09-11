7df428b040acd99d3fa7d85585a507a2eb30ea75
Arthur Vardanyan <arthur@datasfer.com>
2018-09-11 10:00:32 +0400

    expose Ed25519Keccak module.

--- Crypto/NemEd25519.hs
+++ Crypto/NemEd25519.hs
@@ -6,7 +6,7 @@ module Crypto.NemEd25519 (
       module X
     ) where
 
-import Crypto.Error                       as X
-import Crypto.Hash                        as X
-import Crypto.Hash.Algorithms             as X
-import Crypto.PubKey.Ed25519              as X
+import           Crypto.Error                as X
+import           Crypto.Hash                 as X
+import           Crypto.Hash.Algorithms      as X
+import           Crypto.PubKey.Ed25519Keccak as X
